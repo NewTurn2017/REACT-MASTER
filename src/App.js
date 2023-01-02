2054b267f724752962be09eb98a0adaf561dfaa2
jaehyunjang <hyuni2020@gmail.com>
2023-01-02 15:50:36 +0900

    23-01-02: styled Theme selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import styled, { keyframes } from 'styled-components'
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.backgroundColor};
 `
 
 const rotationAnimation = keyframes`
@@ -19,6 +24,12 @@ const rotationAnimation = keyframes`
   }
   `
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`
+const Emoji = styled.span`
+  font-size: 100px;
+`
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -27,23 +38,19 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 100px;
-    &:hover {
-      font-size: 200px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+    font-size: 200px;
   }
 `
 
 function App() {
   return (
     <Wrapper>
+      <Title>Styled Components</Title>
       <Box>
-        <span>😋</span>
+        <Emoji as="p">😋</Emoji>
       </Box>
+      <Emoji>👏</Emoji>
     </Wrapper>
   )
 }
