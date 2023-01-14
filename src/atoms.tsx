aba3d24467ed7eea23530690d2100d6d4241dcd8
jaehyunjang <hyuni2020@gmail.com>
2023-01-14 16:26:24 +0900

    23-01-13: React Beautiful dnd practice

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,18 +1,1 @@
 ﻿import { atom, selector } from 'recoil'
-
-export const minuteState = atom({
-  key: 'minuteState',
-  default: 0,
-})
-
-export const hourSelector = selector({
-  key: 'hourSelector',
-  get: ({ get }) => {
-    const minutes = get(minuteState)
-    return Math.floor(minutes / 60)
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60
-    set(minuteState, minutes)
-  },
-})
